0dd9651a2ecc474dd6665b174f548fd69e72153b
Austin <ALH019@hotmail.com>
2018-09-19 22:33:19 -0400

    Finished up registration and login. Added a GlobalCustomerList. Added some simple error messages for the user when trying to login or make an account.

--- MamaJanePizza/Controllers/LoginController.cs
+++ MamaJanePizza/Controllers/LoginController.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MamaJanePizza.Models;
 
 
 namespace MamaJanePizza.Controllers
 {
     public class LoginController : Controller
     {
-
-        private string userEmail = "";
-        private string password = "";
-        private string firstname = "";
-        private string lastname = "";
-        private string confirmpassword = "";
 
         public IActionResult Index()
         {
@@ -26,33 +21,44 @@ namespace MamaJanePizza.Controllers
             return View();
         }
 
-        public ActionResult UserReg()
+        [HttpPost]
+        public ActionResult Register(CustomerModel customer)
         {
-            userEmail = Convert.ToString(Request.Form["user"]);
-            password = Convert.ToString(Request.Form["password"]);
-            firstname = Convert.ToString(Request.Form["firstname"]);
-            lastname = Convert.ToString(Request.Form["lastname"]);
-            confirmpassword = Convert.ToString(Request.Form["confirmpassword"]);
-
-            if (String.IsNullOrEmpty(userEmail) || String.IsNullOrEmpty(password) || String.IsNullOrEmpty(firstname) || String.IsNullOrEmpty(lastname) || String.IsNullOrEmpty(confirmpassword))
+            if(customer.IsValid(customer))
             {
-                return Redirect("/Login/Register");
+                GlobalCustomerListModel.Add(customer);
+
+                return Redirect("/");
             }
             else
-                return Redirect("/");
+            {
+                ViewBag.Message = "Error! Please Enter Valid Information";
+                return View();
+            }
         }
 
-        public ActionResult AuthUser()
+        [HttpPost]
+        public ActionResult Index(CustomerModel customer)
         {
-            userEmail = Convert.ToString(Request.Form["user"]);
-            password = Convert.ToString(Request.Form["password"]);
-
-            if (String.IsNullOrEmpty(userEmail) || String.IsNullOrEmpty(password))
+            if (String.IsNullOrEmpty(customer.Email) || String.IsNullOrEmpty(customer.Password))
             {
+                ViewBag.Message = "Error! Please Enter Both Email and Password";
                 return Redirect("/Login");
             }
             else
-                return Redirect("/");
+            {
+                CustomerModel user = GlobalCustomerListModel.SearchByEmail(customer.Email);
+                if (user != null)
+                {
+                    GlobalCustomerListModel.CurrentUser = user;
+                    return View();
+                }
+                else
+                {
+                    ViewBag.Message = "Error! Email or Password invalid";
+                    return View();
+                }
+            }
 
         }
     }
---
+++ MamaJanePizza/Models/GlobalCustomerListModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MamaJanePizza.Models
+{
+    //static means we can access this without initialization allowing it to be global and accessed anywhere
+    public static class GlobalCustomerListModel
+    {
+        private static List<CustomerModel> _CustomerList = new List<CustomerModel>();
+        public static CustomerModel CurrentUser { get; set; }
+
+        //Might want to add some logic to prevent adding dups
+        public static void Add(CustomerModel customer)
+        {
+            _CustomerList.Add(customer);
+        }
+
+        public static void RemoveByEmail(string email)
+        {
+            foreach (var item in _CustomerList)
+            {
+                if (item.Email.ToLower() == email.ToLower())
+                {
+                    _CustomerList.Remove(item);
+                    break;
+                }
+            }
+        }
+
+        public static CustomerModel SearchByEmail(string email)
+        {
+            CustomerModel found = null;
+            foreach (var item in _CustomerList)
+            {
+                if (item.Email.ToLower() == email.ToLower())
+                {
+                    found = item;
+                    break;
+                }
+            }
+            return found;
+        }
+
+    }
+}
